7392b124a0621e39acc86787ecc61b06c3e6f410
Mason Stafford <masonstafford00@gmail.com>
2020-03-26 11:05:26 -0400

    adding the range route

--- routes/api.js
+++ routes/api.js
@@ -13,6 +13,15 @@ module.exports = (app) => {
     });    
   });
 
+  app.get("/api/workouts/range", (req, res) => {
+    db.Workout.find({}).then(dbWorkout => {
+      res.json(dbWorkout);
+    })
+    .catch(err => {
+      res.json(err)
+    })
+  });
+
   app.put("/api/workouts/:id", (req, res) => {
     db.Workout.findByIdAndUpdate(req.params.id,
     {$push: {exercises: req.body}}, 
